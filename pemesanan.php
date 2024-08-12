bbfb6f47e9d69ab3056c1de12e4159a4ff8ffdcb
arisuryaa <arisurya228@gmail.com>
2024-08-13 02:33:16 +0800

    menampilkan data produk dari singgle-produk.php ke pemesanan.php

--- pemesanan.php
+++ pemesanan.php
@@ -2,6 +2,11 @@
 
 $data_barang = $_POST;
 
+// var_dump($data_barang);
+
+// if(isset($_POST["submit"])) {
+//     var_dump($_POST);
+// }
 
 ?>
 
@@ -14,22 +19,15 @@ $data_barang = $_POST;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <title>Bali Shop</title>
-    <!-- <style>
-        .kotak {
-    border: 1px solid black;
+    <style>
 
-  }
-    </style> -->
+    </style>
 </head>
 
 <body>
-
-    <!--navbar-->
-
     <nav>
         <div class="logo">
             <a href="index.php"><img src="asset/img/logo.svg" alt="Logo"></a>
@@ -46,17 +44,112 @@ $data_barang = $_POST;
         </div>
     </nav>
 
-    <!--hero-->
-
+    <!-- Breadcrumbs -->
     <div class="hero">
         <h1>
             Checkout <br>
         </h1>
-
         <div class="tombol">
             <a href="index.php">Home</a>
         </div>
     </div>
+
+
+    <section>
+        <form action="" method="POST">
+            <div class="container-utama">
+
+                <div class="container-kiri">
+                    <h1>Detail Tagihan</h1>
+                    <div class="data-customer">
+                        <div class="namaCust">
+                            <label for="nama">Nama Lengkap</label>
+                            <input type="text" name="nama" id="nama">
+                        </div>
+                        <div class="nomorCust">
+                            <label for="nomor">Nomor Telpon</label>
+                            <input type="number" name="telpon" id="nomor">
+                        </div>
+                    </div>
+                    <div class="catatan">
+                        <h1>Informasi Tambahan</h1>
+                        <div class="catatanCust">
+                            <label for="nama">Catatan Pesanan</label>
+                            <input type="text" name="catatan" id="nama">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-kanan">
+                    <div class="pesananAnda">
+                        <h1>Pesanan Anda</h1>
+                    </div>
+                    <div class="dataPesanan">
+                        <div class="namaProduk">
+                            <div class="titleProduk">
+                                <h1>Produk</h1>
+                            </div>
+                            <div class="dataProduk">
+                                <h1><?= $data_barang["judul_barang"] ?></h1>
+                                <h1> x<?= $data_barang["quantity_barang"] ?></h1>
+                            </div>
+                        </div>
+                        <div class="totalProduk">
+                            <div class="totalProduk">
+                                <h1>Subtotal</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <button>Buat Nomor Pesanan</button>
+                </div>
+
+            </div>
+        </form>
+
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+
+
+
+    <!-- <section id="home">
+        <div class="container-utama">
+            <div class="container-kiri">
+                <h1>Detail Tagihan</h1>
+                <div class="form-kiri">
+                    <form action="#">
+                        <div class="row">
+                            <div class="mb-3 col-6">
+                                <label for="jurusan" class="form-label">Nama Lengkap</label>
+                                <input type="text" class="form-control" id="firstname" name="firstname"
+                                    placeholder="....">
+                            </div>
+
+                            <div class="mb-3 col-6">
+                                <label for="jk" class="form-label">Telepon</label>
+                                <input type="text" class="form-control" id="firstname" name="firstname"
+                                    placeholder="....">
+                            </div>
+                        </div>
+                        <h1>Informasi Tambahan</h1>
+                        <input class="catatan" type="text" placeholder="catatan">
+                    </form>
+                </div>
+            </div>
+            <div class="container-kanan">
+                <h1>Pesanan Anda</h1>
+            </div>
+        </div>
+    </section> -->
+
+
+
+
+
 
     <!--detail tagihan-->
 
@@ -125,39 +218,3 @@ $data_barang = $_POST;
     </div>
 
 </div> -->
-
-
-    <section id="home">
-        <div class="container-utama">
-            <div class="container-kiri">
-                <h1>Detail Tagihan</h1>
-                <div class="form-kiri">
-                    <form action="#">
-                        <div class="row">
-                            <div class="mb-3 col-6">
-                                <label for="jurusan" class="form-label">Nama Lengkap</label>
-                                <input type="text" class="form-control" id="firstname" name="firstname"
-                                    placeholder="....">
-                            </div>
-
-                            <div class="mb-3 col-6">
-                                <label for="jk" class="form-label">Telepon</label>
-                                <input type="text" class="form-control" id="firstname" name="firstname"
-                                    placeholder="....">
-                            </div>
-                        </div>
-                        <h1>Informasi Tambahan</h1>
-                        <input class="catatan" type="text" placeholder="catatan">
-                    </form>
-                </div>
-            </div>
-            <div class="container-kanan">
-                <h1>Pesanan Anda</h1>
-            </div>
-        </div>
-    </section>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
